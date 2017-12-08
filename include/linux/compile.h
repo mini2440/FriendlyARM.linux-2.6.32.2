a4f8c51d4bb13b8d356bbd037e3208702d7a3e7d
mz8023yt <mz8023yt@163.com>
2017-12-09 00:25:30 +0800

    feature: make the kernel

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 1 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 Fri Dec 8 23:47:44 CST 2017"
+#define LINUX_COMPILE_TIME "23:47:44"
+#define LINUX_COMPILE_BY "user"
+#define LINUX_COMPILE_HOST "vmware"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.3 (ctng-1.6.1) "
